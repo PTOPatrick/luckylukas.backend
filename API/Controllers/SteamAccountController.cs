3ea5b8ab5812f6988558c0608bbc5c804d1533f9
PTOPatrick <patrick.toma@gmx.ch>
2022-12-18 22:38:02 +0100

    added authorization in backend

--- API/Controllers/SteamAccountController.cs
+++ API/Controllers/SteamAccountController.cs
@@ -1,9 +1,11 @@
 using API.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class SteamAccountController : ControllerBase
@@ -12,7 +14,7 @@ namespace API.Controllers
         {
             // tbd
         }
-
+        
         [HttpGet]
         public ActionResult<IEnumerable<SteamAccountDto>> GetSteamAccounts() {
             var accounts = new List<SteamAccountDto>();
@@ -20,6 +22,38 @@ namespace API.Controllers
                 Name = "Name1",
                 CsRank = "Silver 1",
                 Owner = "LuckyLukas 1",
+                Created = DateTime.UtcNow,
+                Changed = DateTime.UtcNow,
+                IsBanned = false
+            });
+            accounts.Add(new SteamAccountDto {
+                Name = "Name2",
+                CsRank = "Silver 2",
+                Owner = "LuckyLukas 3",
+                Created = DateTime.UtcNow,
+                Changed = DateTime.UtcNow,
+                IsBanned = false
+            });
+            accounts.Add(new SteamAccountDto {
+                Name = "Name3",
+                CsRank = "Silver 3",
+                Owner = "LuckyLukas 3",
+                Created = DateTime.UtcNow,
+                Changed = DateTime.UtcNow,
+                IsBanned = false
+            });
+            accounts.Add(new SteamAccountDto {
+                Name = "Name4",
+                CsRank = "Silver 4",
+                Owner = "LuckyLukas 4",
+                Created = DateTime.UtcNow,
+                Changed = DateTime.UtcNow,
+                IsBanned = false
+            });
+            accounts.Add(new SteamAccountDto {
+                Name = "Name5",
+                CsRank = "Silver 5",
+                Owner = "LuckyLukas 5",
                 Created = DateTime.UtcNow,
                 Changed = DateTime.UtcNow,
                 IsBanned = false
